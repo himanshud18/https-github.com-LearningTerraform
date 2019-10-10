dec7db4f54c58c20284c947d5415470446ed0ccf
vvijay777 <sftesting7777@gmail.com>
2019-10-10 17:28:00 +0530

    updated the outbound

--- main.tf
+++ main.tf
@@ -42,4 +42,10 @@ resource "aws_security_group" "default" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }    
 }
