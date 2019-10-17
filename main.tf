41cef41a083649a85a6155cf27a086886f692fb6
vvijay777 <sftesting7777@gmail.com>
2019-10-17 20:04:00 +0530

    Updated the colour

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ resource "aws_instance" "default" {
   #! /bin/bash
       sudo yum -y install httpd
       sudo systemctl start httpd
-      echo "<html><body style=background-color:powderblue;>Refinitiv Lab</body></html>" | sudo tee /var/www/html/index.html
+      echo "<html><body style=background-color:red;>Refinitiv Lab</body></html>" | sudo tee /var/www/html/index.html
       EOF
 }
 
