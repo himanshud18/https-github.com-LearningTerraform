a0733b267349d3c8c47cad674f255c2bfa7e48f8
dadheechh <33571788+dadheechh@users.noreply.github.com>
2019-10-18 15:31:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 # Terraform state will be stored in S3
 terraform {
   backend "s3" {
-    bucket = "terraform-bucket-vijith-test"
+    bucket = "hd-aws-bucket"
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
